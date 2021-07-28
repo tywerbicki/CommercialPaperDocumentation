08e209411fbf2d4ffc7b54ead55732ace8cdd7fd
tywerbicki <86994333+tywerbicki@users.noreply.github.com>
2021-07-28 11:50:04 -0600

    Create CPDatabaseUpdater.bat

---
+++ CPDatabaseUpdater.bat
@@ -0,0 +1,9 @@
+set root=<directory containing Anaconda3>
+
+call %root%\Anaconda3\Scripts\activate.bat %root%\Anaconda3
+
+cd <directory containing CPDatabaseUpdater.py>
+
+python "CPDatabaseUpdater.py"
+
+pause
